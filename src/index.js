cc87fefe8263cb835c1c8b5b84f5e9df532c268e
marcelo alejnadro herlan <alelabestiadekiev@gmail.com>
2022-01-16 14:16:47 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { render } from "react-dom";
+import React from "react";
+
+function App() {
+  return <div className="main">hola chiques</div>;
+}
+
+render(<App />, document.getElementById("root"));
